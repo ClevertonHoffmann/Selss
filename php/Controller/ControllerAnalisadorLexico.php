943021c43c2e27458fc26d9488d9257d594a38fc
Cleverton Hoffmann <41930262+ClevertonHoffmann@users.noreply.github.com>
2023-12-01 17:31:06 -0300

    Finalização parte léxica 2
    Falta verificar os erros.

--- php/Controller/ControllerAnalisadorLexico.php
+++ php/Controller/ControllerAnalisadorLexico.php
@@ -3,14 +3,15 @@
 /*
  * Classe responsável pela analise léxica e retorno dos dados de análise léxica do código digitado pelo usuário
  */
+
 //require_once '../php/Persistencia/PersistenciaAnalisadorLexico.php';
 
-class ControllerAnalisadorLexico extends Controller{
+class ControllerAnalisadorLexico extends Controller {
 
     public function __construct() {
         $this->carregaClasses('AnalisadorLexico');
     }
-        
+
     //Variáveis e instancias iniciais carregegadas no construtor
     public array $aPalavrasReservadas;
     public array $aTabelaDeTransicao;
@@ -28,7 +29,7 @@ class ControllerAnalisadorLexico extends Controller{
      * @return string
      */
     public function InicializaAnalisadorLexico($sTexto) {
-        
+
         $this->aPalavrasReservadas = $this->oPersistencia->retornaPalavrasReservadas();
         $this->aTabelaDeTransicao = $this->oPersistencia->retornaTabelaDeTransicao();
         $this->aTabelaTokens = $this->oPersistencia->retornaTabelaDeTokens();
@@ -56,12 +57,20 @@ class ControllerAnalisadorLexico extends Controller{
         $iK = 0;
         while ($this->iCount > 0) {
             try {
-                //Aceita o caractere e avança uma posição na entrada caso for espaço valida como tal
-                if (!((($this->aTabelaDeTransicao[$this->q][$this->aCaracteresSeparados[$iK]]) == '-1')||(($this->aTabelaDeTransicao[$this->q]['\''.$this->aCaracteresSeparados[$iK].'\'']) == '-1')) && isset($this->aCaracteresSeparados[$iK])) {
-                    //Concatena até formar um token
-                    $this->sBuild .= $this->aCaracteresSeparados[$iK];
-                    //Seta o estado presente na tabela
-                    $this->q = (int) $this->aTabelaDeTransicao[$this->q][$this->aCaracteresSeparados[$iK]];
+                //Aceita o caractere e avança uma posição na entrada tanto normal como com espaços
+                if (!((($this->aTabelaDeTransicao[$this->q][$this->aCaracteresSeparados[$iK]]) == '-1') || (($this->aTabelaDeTransicao[$this->q]["'".$this->aCaracteresSeparados[$iK]."'"]) == '-1')) && isset($this->aCaracteresSeparados[$iK])) {
+                    //Estado com espaços
+                    if ($this->aCaracteresSeparados[$iK] == " ") {
+                        //Concatena até formar um token
+                        $this->sBuild .= "'".$this->aCaracteresSeparados[$iK]."'";
+                        //Seta o estado presente na tabela
+                        $this->q = (int) $this->aTabelaDeTransicao[$this->q]["'".$this->aCaracteresSeparados[$iK]."'"];
+                    } else {
+                        //Concatena até formar um token
+                        $this->sBuild .= $this->aCaracteresSeparados[$iK];
+                        //Seta o estado presente na tabela
+                        $this->q = (int) $this->aTabelaDeTransicao[$this->q][$this->aCaracteresSeparados[$iK]];
+                    }
                     $this->iCount--;
                     $iK++;
                     //Aceita o token
@@ -87,15 +96,17 @@ class ControllerAnalisadorLexico extends Controller{
         }
         $aListaTokenLexPer = array();
         $aListaTokenLexPer[0] = ['Token', 'Lex', 'Pos'];
-       // $this->aListadeTokensLex[] = [$this->aTabelaTokens[$this->q], $this->sBuild, $this->qntTokens];
+        if ($this->qntTokens == 0) {
+            $this->aListadeTokensLex[] = [$this->aTabelaTokens[$this->q], $this->sBuild, $this->qntTokens];
+        }
         $sTeste = "Token    Lex    Pos \\n ";
         $sTextoRetorno = '{"texto":';
         foreach ($this->aListadeTokensLex as $aLex) {
             $sTeste .= "" . $aLex[0] . "    " . $aLex[1] . "         " . $aLex[2] . " \\n ";
-            $aListaTokenLexPer[] = [$aLex[0],$aLex[1],$aLex[2]];
+            $aListaTokenLexPer[] = [$aLex[0], $aLex[1], $aLex[2]];
         }
         $this->oPersistencia->gravaResultadoAnaliseLexica($aListaTokenLexPer);
-        
+
         $sTextoRetorno .= '"' . $sTeste . '"}';
         return json_encode($sTextoRetorno);
     }
@@ -105,15 +116,14 @@ class ControllerAnalisadorLexico extends Controller{
      * @param type $sDados
      * @return type
      */
-    public function mostraModalResultadoAnaliseLexica($sDados){
+    public function mostraModalResultadoAnaliseLexica($sDados) {
 
-        $aTabela = $this->oPersistencia->retornaArrayCSV("resultadoAnaliseLexica.csv", 1); 
+        $aTabela = $this->oPersistencia->retornaArrayCSV("resultadoAnaliseLexica.csv", 1);
         $sModal = $this->oView->geraModalResAnaliseLexica($aTabela);
 
         return json_encode($sModal);
     }
-    
-   
+
 //    /*
 //     * Método responsável por realizar a análise léxica do código
 //     */
@@ -168,5 +178,4 @@ class ControllerAnalisadorLexico extends Controller{
 //        $sTextoRetorno .= '"' . $sTeste . '"}';
 //        return json_encode($sTextoRetorno);
 //    }
-
 }
--- php/View/ViewAnalisadorLexico.php
+++ php/View/ViewAnalisadorLexico.php
@@ -1,21 +1,12 @@
 <?php
 
-class ViewAnalisadorLexico{
-    
+class ViewAnalisadorLexico {
+
     /**
      * Recebe como parâmetro o array que veio do csv
      * @return string
      */
     public function geraModalResAnaliseLexica($aCsvArray) {
-
-        $aCores = array(
-            '#CCFFCC', '#C2E3C6', '#B3E6CC', '#B2EBF2', '#CCFFFF', '#F0F4C3', '#E6E6FA', '#FFDAB9', '#E0FFFF', '#F0FFF0',
-            '#E0EEE0', '#F0FFF0', '#F0FFF0', '#F5FFFA', '#FAFAD2', '#F5F5DC', '#FFFFF0', '#FFF5EE', '#FFFAFA', '#FFFFE0',
-            '#FAEBD7', '#FFF0F5', '#FFFACD', '#FFFFF0', '#FFF8DC', '#FFEBCD', '#FFFAF0', '#FFE4E1', '#FFDEAD', '#FFF8E7',
-            '#FFF0F5', '#FFFAFA', '#FFFFF0', '#FFF5EE', '#FFFACD', '#FFEFD5', '#FFE4B5', '#FFEFDB', '#FFDAB9', '#FFE4C4',
-            '#FFF5EE', '#FFF8DC', '#FFFAF0', '#FFFACD', '#FFEBCD', '#FFDAB9', '#FFE4E1', '#FFDEAD', '#FFEFD5', '#FFE4B5',
-            '#FFEFDB', '#FFDAB9'
-        );
 
         $sHtmlModal = '<table class="modal-table">';
         $iK = 0;
@@ -26,11 +17,7 @@ class ViewAnalisadorLexico{
                 $sHtmlModal .= '<tr>';
             }
             foreach ($row as $cell) {
-                if (is_int((int) htmlspecialchars($cell)) && (int) htmlspecialchars($cell) > 0 && $iK != 0) {
-                    $sHtmlModal .= '<td class="modal-table td" style="background-color: ' . $aCores[(int) htmlspecialchars($cell)] . ';">' . htmlspecialchars($cell) . '</td>';
-                } else {
-                    $sHtmlModal .= '<td class="modal-table td">' . htmlspecialchars($cell) . '</td>';
-                }
+                $sHtmlModal .= '<td class="modal-table td">' . htmlspecialchars($cell) . '</td>';
             }
             $iK++;
             $sHtmlModal .= '</tr>';
@@ -39,7 +26,7 @@ class ViewAnalisadorLexico{
 
         $sDiretorio = $_SESSION['diretorio'];
 
-        $arquivo = $sDiretorio."//modal.html";
+        $arquivo = $sDiretorio . "//modal.html";
 
         //Variável $fp armazena a conexão com o arquivo e o tipo de ação.
         $fp = fopen($arquivo, "w");
@@ -52,5 +39,5 @@ class ViewAnalisadorLexico{
 
         return $sHtmlModal;
     }
-    
+
 }
